f95b2bb7aa5742fd262bcb7e3c1b634746cc2dab
amaananwar46 <amaananwar46@gmail.com>
2025-11-23 17:00:16 +0530

    error solved 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react'
-import NavBar  from './components/NavBar'
+import NavBar from "./components/NavBar.jsx";
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Settings from './components/Settings'
