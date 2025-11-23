d05327b96856d28f2a74f8487ae10756511ff1ef
amaananwar46 <amaananwar46@gmail.com>
2025-11-23 17:17:03 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react'
-import Navbar from "./components/Temp.jsx"
+import Temp from "./components/NavBar.jsx"
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Settings from './components/Settings'
