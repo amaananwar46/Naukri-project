1d3d551c3b509266606644f3525c987562046f1c
amaananwar46 <amaananwar46@gmail.com>
2025-11-24 22:25:49 +0530

    toolkit error 2

--- src/Toolkit/store.js
+++ src/Toolkit/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./LoginSlice"
-import applicationHistory from "./applicationSlice"
-// import ApplicationHistory from "../components/ApplicationHistory";
+
+    
+import applicationHistory from "../applicationSlice"
 
   const store= configureStore({
     reducer:{
